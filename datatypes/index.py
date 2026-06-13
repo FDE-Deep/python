6c04419291f08b9bbe54b90eb0d6e84598ccb077
Deep Singh <fde.deep@gmail.com>
2026-06-13 11:50:26 +0530

    datatypes in python

---
+++ datatypes/index.py
@@ -0,0 +1,73 @@
+# Following are the data types in python
+# 1. int
+# 2. float
+# 3. str
+# 4. None
+# 5. bool
+# 6. list
+# 7. tuple
+# 8. set
+# 9. dict
+# 10. complex
+
+# Example of int
+
+num = 10
+print(type(num)) #<class 'int'>
+
+# Example of float
+
+num = 10.5
+print(type(num)) #<class 'float'>
+
+# Example of str
+
+name = "Hello World"
+print(type(name)) #<class 'str'>
+
+# Example of None
+
+value = None
+print(type(value)) #<class 'NoneType'>
+
+# Example of bool
+
+flag = True
+print(type(flag)) #<class 'bool'>
+
+flag = False
+print(type(flag)) #<class 'bool'>
+
+# Example of list
+
+my_list = [1, 2, 3]
+print(type(my_list)) #<class 'list'>
+
+# Example of tuple
+
+my_tuple = (1, 2, 3)
+print(type(my_tuple)) #<class 'tuple'>
+
+# Example of set
+
+my_set = {1, 2, 3}
+print(type(my_set)) #<class 'set'>
+
+# Example of dict
+
+my_dict = {'name': 'John', 'age': 30}
+print(type(my_dict)) #<class 'dict'>
+
+# Example of complex
+
+num = 1 + 2j
+print(type(num)) #<class 'complex'>
+
+# Boolean can be converted to int. True is 1 and False is 0
+
+flag = True
+print(int(flag)) # 1
+
+flag = False
+print(int(flag)) # 0
+
